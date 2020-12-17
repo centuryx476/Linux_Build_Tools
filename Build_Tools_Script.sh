26fa199a625422065b4a33cafedf0b8f66c888c0
centuryx476 <helpdesk@sandwp.com>
2020-12-17 09:41:41 -0500

    Master_Branch_Commit_01
    Adding first Build bash script

---
+++ Build_Tools_Script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Install make
+sudo apt-get install build-essential
+
+# Install libssl
+sudo apt-get install libssl-dev
+
+# Install GNU Compiler
+sudo apt install gcc-10 g++-10
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
+
+# Install CMake
+version=3.19
+build=2
+mkdir ~/temp
+cd ~/temp
+wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
+tar -xzvf cmake-$version.$build.tar.gz
+cd cmake-$version.$build/
+./bootstrap
+make
+sudo make install
+
+
+
+
+
+
+# End of Line
