ed086005dd9922cd97fb90217d4f10a7e0a07288
centuryx476 <helpdesk@sandwp.com>
2020-12-17 15:07:20 -0500

    Master_Branch_Commit_06

---
+++ Scripts/Initial_Install_Script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Set TimeZone
+timedatectl set-timezone America/New_York
+
+# Disable Automatic Date/Time
+timedatectl set-ntp 0
+
+# Install Zip
+apt-get install zip
+
+#Install rsync
+apt-get install rsync
+
+# Install Updates
+apt-get update && sudo apt-get upgrade
+
+
+
+# End of Line
