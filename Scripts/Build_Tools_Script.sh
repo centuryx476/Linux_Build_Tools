13c5424963a0b55c321662e637af101fd2cb3b60
centuryx476 <helpdesk@sandwp.com>
2020-12-17 10:21:54 -0500

    Master_Branch_Commit_03

--- Scripts/Build_Tools_Script.sh
+++ Scripts/Build_Tools_Script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Install make
-sudo apt-get install build-essential
+apt-get install build-essential
 
 # Install libssl
-sudo apt-get install libssl-dev
+apt-get install libssl-dev
 
 # Install GNU Compiler
-sudo apt install gcc-10 g++-10
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
+apt install gcc-10 g++-10
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
 
 # Install CMake
 version=3.19
@@ -20,7 +20,7 @@ tar -xzvf cmake-$version.$build.tar.gz
 cd cmake-$version.$build/
 ./bootstrap
 make
-sudo make install
+make install
 
 
 
