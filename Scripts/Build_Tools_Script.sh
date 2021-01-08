79ae784f2b0ebccff7934bd95377562c6094b44e
centuryx476 <SNoorizadeh@SandWP.com>
2021-01-08 14:25:23 -0500

    Master_Branch_Commit_08

--- Scripts/Build_Tools_Script.sh
+++ Scripts/Build_Tools_Script.sh
@@ -7,8 +7,11 @@ apt-get install build-essential
 apt-get install libssl-dev
 
 # Install GNU Compiler
-apt install gcc-10 g++-10
+apt-get install gcc-10 g++-10 gdb
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10 --slave /usr/bin/gcov gcov /usr/bin/gcov-10
+
+# Install Ninja-Build
+apt-get install ninja-build
 
 # Install CMake
 version=3.19
@@ -21,9 +24,6 @@ cd cmake-$version.$build/
 ./bootstrap
 make
 make install
-
-
-
 
 
 
